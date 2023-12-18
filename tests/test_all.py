4845e2e698504503b816c4f5710dda19291b2286
Leon <llw2128@columbia.edu>
2023-12-18 12:37:25 -0500

    added test

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,16 +1,9 @@
-from CB_IPO import scrape
+from resources import StudentsResource
 import pytest as pt
 from pytest import mark
 
-@mark.parametrize(
-    "input,output",
-    [
-        (5, "https://www.sec.gov/edgar/search/#/filter_forms=S-1&page=5"),
-        (4, "https://www.sec.gov/edgar/search/#/filter_forms=S-1&page=4"),
-    ],
-)
-def test_page_set_mult(input, output):
-    tester.reset_url()
-    tester.set_page(input)
-    assert tester.set_page(input) == output
+tester = StudentsResource()
+
+def test_init():
+    assert tester.get_students() is None
 
