13bdaa43ac1fa62195f5ad3c3431eefd10a2b28c
Leon <llw2128@columbia.edu>
2023-12-18 23:44:20 -0500

    remove environment

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-sudo pip3 install virtualenv
 cd /home/ec2-user/Cloud7Students
-virtualenv environment
-source environment/bin/activate
 sudo pip3 install -r requirements.txt
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/Cloud7Students/
-#source environment/bin/activate
-#supervisord -c supervisord.conf
 nohup python3 /home/ec2-user/Cloud7Students/main.py > output.log 2>&1 &
